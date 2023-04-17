4a012aaf96358a901f82d89a024c4bdf81b5398c
Bashir Manchala <bashirmanchala@KAMINT.com>
2023-04-17 06:42:39 +0500

    added changes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const movies = [
 {
-title: "Avengers: Endgame",
+title: "Captain America: Civil War ",
 genre: "action",
-year: 2020,
-image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
+year: 2016,
+image: "https://jmstevenson.files.wordpress.com/2016/05/marvel-civil-war-alternate-poster.jpg?w=838"
 },
+{
+   title: "Spider-Man: No Way Home",
+   genre: "action",
+   year: 2021,
+   image: "https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2021/12/20/3297b6ad-5b32-4e86-96c5-f82c5275fbe8_rw_1920_1.jpg"
+   },
 {
 title: "Joker",
 genre: "drama",
-year: 2022,
+year: 2019,
 image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
 },
 {
@@ -18,22 +24,22 @@ year: 2019,
 image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qG59J1Q7rpBc1dvku4azbzcqo8h.jpg"
 },
 {
-title: "Us",
+title: "The Nun",
 genre: "horror",
 year: 2018,
-image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mvDVOYluQwcyfqTITJcT2gVesjP.jpg"
+image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQKiSaP9uVzCb2clUf2eUAgkf_uzj8KMYs0g1hGvJP-SsqB6A0B"
 },
 {
-title: "The Notebook",
-genre: "romance",
-year: 2021,
-image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/k0oN89q8K1zll9OsHk5NWmHZZGI.jpg"
+title: "Harry Potter And The Cursed Child",
+genre: "thrill",
+year: 2016,
+image: "https://upload.wikimedia.org/wikipedia/en/thumb/8/87/Cursed_Child_new_poster.jpg/220px-Cursed_Child_new_poster.jpg"
 },
 {
-title: "Inception",
-genre: "sci-fi",
-year: 2019,
-image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"
+title: "The Jungle Book",
+genre: "fantacy",
+year: 2016,
+image: "https://m.media-amazon.com/images/M/MV5BMTc3NTUzNTI4MV5BMl5BanBnXkFtZTgwNjU0NjU5NzE@._V1_.jpg"
 },
 ];
 
